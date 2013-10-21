560c305d6afdee1c012b43c47eb132ec7fc77b6b
Henrik Olsen <henrik@mouseketeers.dk>
2013-10-21 14:49:31 +0200

    New settings features: Number of events to show per page, Scope (section, subsections, entire site), View upcoming, previous (for archive) or both

--- code/EventsSection.php
+++ code/EventsSection.php
@@ -4,78 +4,91 @@ class EventsSection extends Page {
 		'Pagination' => 'Boolean',
 		'NumPages' => 'Int',
 		'ExcludeOutdated' => 'Boolean',
-		'NoEventsText' => 'Text'
+		'Scope' => 'Enum("section,subsections,site","section")',
+		'DateFilter' => 'Enum("upcoming,previous,nofilter","upcoming")'
 	);
 	static $has_one = array();
-	//static $allowed_children = array('EventPage');
 	static $defaults = array(
 		'SortOrder' => 'FromDate DESC',
 		'ExcludeOutdated' => '1'
 	);
  	static $default_child = 'EventPage';
  	static $icon  = 'events/images/eventssection';
- 	
+  	static $num_pages_options = array(
+		0 => 'All',
+		2 => '2',
+		3 => '3',
+		4 => '4',
+		5 => '5',
+		6 => '6',
+		7 => '7',
+		8 => '8',
+		9 => '9',
+		10 => '10',
+		15 => '15',
+		20 => '20',
+		50 => '50',
+		100 => '100'
+	);
 	function getCMSFields() {
 		$fields = parent::getCMSFields();
-		$fields->addFieldToTab(
-			'Root.Content.Settings', new LiteralField(
-				$name = 'SlideshowSettingsHeader',
-	   			$content = '<h3>'._t('EventsSection.SETTINGSHEADER', 'Events Section Settings').'</h3>'
+		$fields = parent::getCMSFields();
+		$fields->addFieldsToTab('Root.Content.Settings', 
+			array(
+				new LiteralField('SectionSettingHeader', '<br /><h3>'._t('Section.SETTINGSHEADER', 'Section Settings').'</h3>'),
+				new DropdownField('NumPages','Number of events to list',self::$num_pages_options),
+				new OptionsetField('Scope','Scope',
+					$source = array(
+						'section' => 'Show events in this section',
+						'subsections' => 'Show events in this section and all of its subsections',
+						'site' => 'Show all events from the entire site'
+					)
+				),
+				new OptionsetField('DateFilter','Date filter',
+					$source = array(
+						'upcoming' => 'Show upcoming events only',
+						'previous' => 'Show previous events only (use this for event archives)',
+						'nofilter' => 'Show both upcoming and previous events',
+					)
+				)
 			)
 		);
-		$fields->addFieldToTab(
-			$tab = 'Root.Content.Settings',
-			new NumericField(
-				$db_name = 'NumPages',
-				$cms_label = _t('EventSection.MAX_PAGES_TO_LIST','Max pages to list (set to 0 to list all pages)')
-			),
-			$place_before = ''
-		);
-		$fields->addFieldToTab(
-			$tab = 'Root.Content.Settings',
-			new CheckboxField(
-				$db_name = 'ExcludeOutdated',
-				$cms_label = _t('EventSection.EXCLUDE_OUTDATED','Exclude outdated events from the list')
-			),
-			$place_before = ''
-		);
-		$fields->addFieldToTab(
-			$tab = 'Root.Content.Settings',
-			new TextAreaField(
-				$db_name = 'NoEventsText',
-				$cms_label = _t('EventSection.NoEventsText','Text to show when there are no events')
-			),
-			$place_before = ''
-		);
 		return $fields;
-	}
-	
-	public function Events($num='', $parent='', $sort='FromDate DESC') {
-		$where = '(ToDate IS NULL OR ToDate >= NOW()) AND (FromDate IS NULL OR FromDate <= NOW())';
-		if ($parent) $where .= ' AND ParentID = '.$this->ID;
-		return DataObject::get('EventPage', $where, $sort, '', $num);
-		//return DataObject::get('EventPage', 'ParentID = '.$this->ID.' AND (FromDate > NOW())', 'FromDate DESC', '', $num);
-	}
-	public function UpcomingEvents($num='', $parent='', $sort='FromDate DESC') {
-		$where = 'FromDate > NOW() AND IsWebEvent = '.$is_web_event;
-		if ($parent) $where .= ' AND ParentID = '.$this->ID;
-		return DataObject::get('EventPage', $where, $sort, '', $num);
 	}
 
 }
 class EventsSection_Controller extends Page_Controller {
 	public function ContentList() {
+		$scope_filter = '';
+		$date_filter = '';
 		$limit = '';
 		if($this->NumPages) {
 			if(!isset($_GET['start']) || !is_numeric($_GET['start']) || (int)$_GET['start'] < 1) $_GET['start'] = 0;
 			$limit_start = (int)$_GET['start'];
 			$limit = $limit_start.','.$this->NumPages;
 		}
-		$filter = 'ParentID = '. $this->ID;
-		//if ($this->ExcludeOutdated) $filter .= ' AND (ToDate IS NULL OR ToDate >= NOW()) AND (FromDate IS NULL OR FromDate <= NOW())';
-		if ($this->ExcludeOutdated) $filter .= ' AND ToDate IS NULL OR ToDate >= NOW()';
+		switch($this->Scope) {
+			case 'subsections':
+				$decendent_ids = $this->getDescendantIDList();
+				$scope_filter = '"EventPage"."ID" IN ('.implode(',',$decendent_ids).') ';
+				break;
+
+			case 'section':
+				$scope_filter = 'ParentID = '. $this->ID;
+				break;
+		}
+		switch($this->DateFilter) {
+			case 'upcoming':
+				$date_filter = '(FromDate IS NULL OR FromDate >= NOW() - INTERVAL 1 DAY) AND (ToDate IS NULL OR ToDate >= NOW() - INTERVAL 1 DAY)';
+				break;
+
+			case 'previous':
+				$date_filter = '(FromDate < NOW() - INTERVAL 1 DAY) AND (ToDate IS NULL OR ToDate < NOW() - INTERVAL 1 DAY)';
+				break;
+		}
+		$filter = ($scope_filter) ? $scope_filter.' AND '.$date_filter : $date_filter;
+
 		$data = DataObject::get('EventPage', $filter, 'FromDate DESC','',$limit);
 		return $data;
 	}
 }
-?>
